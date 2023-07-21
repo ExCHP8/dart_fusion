13c822670d0e5c4e6e1b499e760308357ee9bec1
Nialixus <nialixus@gmail.com>
2023-07-21 17:01:16 +0700

    Finishing v1.0.0

--- README.md
+++ README.md
@@ -1,39 +1,47 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# AppStateWidget
+Making `StatefulWidget` prettier and less boilerplate.
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
-
-## Features
-
-TODO: List what your package can do. Maybe include images, gifs, or videos.
-
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
-
-```dart
-const like = 'sample';
+## Installation
+put this in your pubspec.yaml
+```yaml
+dependencies:
+  appstate_widget:
+    git:
+      url: https://github.com/Nialixus/Appstate-Widget.git
 ```
 
-## Additional information
+## Usage
+To use it, you're going to need extending this class like this
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+```dart
+class MyClass extends AppStateWidget {
+    const MyClass({super.key});
+
+    @override
+    Map<String, dynamic> data(AppStateValue state){
+    return {"controller": TextEditingController()};
+    }
+
+    @override
+    void onPreparation(AppStateValue state){
+    state.data['controller'].text = 'Loading';
+    }
+
+    @override
+    Widget onStart(AppStateValue state){
+    return TextField(controller: state.data['controller']);
+    }
+
+    void onReady(AppStateValue state){
+    state.data['controller'].text = 'Loaded';
+    }
+
+    @override
+    void onFinish(AppStateValue state){
+    state.data['controller'].dispose();
+    }
+}
+```
+
+## Documentation
+[Dart generated doc](https://github.com/Nialixus/Appstate-Widget/tree/main/doc/api/index.html "AppStateWidget Documentation")
--- lib/appstate_widget.dart
+++ lib/appstate_widget.dart
@@ -1,7 +1,86 @@
+/// Making [StatefulWidget] prettier and less boilerplate.
 library appstate_widget;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+/// Making [StatefulWidget] prettier and less boilerplate.
+///
+/// Though this doesn't support [AppStateValue.setState] one of the suggested
+/// workaround is making an extended class of [ValueNotifier] and build it with
+/// [ValueListenableBuilder].
+class AppStateWidget extends StatefulWidget {
+  /// Default constant constructor with one field of [key].
+  ///
+  /// To use it, you're going to need extending this class
+  /// ```dart
+  /// class MyClass extends AppStateWidget {
+  ///   const MyClass({super.key});
+  ///
+  ///   @override
+  ///   Map<String, dynamic> data(AppStateValue state){
+  ///     return {"controller": TextEditingController()};
+  ///   }
+  ///
+  ///   @override
+  ///   void onPreparation(AppStateValue state){
+  ///     state.data['controller'].text = 'Loading';
+  ///   }
+  ///
+  ///   @override
+  ///   Widget onStart(AppStateValue state){
+  ///     return TextField(controller: state.data['controller']);
+  ///   }
+  ///
+  ///   void onReady(AppStateValue state){
+  ///     state.data['controller'].text = 'Loaded';
+  ///   }
+  ///
+  ///   @override
+  ///   void onFinish(AppStateValue state){
+  ///     state.data['controller'].dispose();
+  ///   }
+  /// }
+  /// ```
+  const AppStateWidget({super.key});
+
+  /// Overriding [AppStateValue.build] inside [createState].
+  Widget onStart(AppStateValue state) => const Placeholder();
+
+  /// A map to declare and passing data inside class. This make things cleaner.
+  Map<String, dynamic> data(AppStateValue state) => {"message": "Hello World"};
+
+  /// Overriding [AppStateValue.dispose] inside [createState].
+  void onFinish(AppStateValue state) {}
+
+  /// Overriding [AppStateValue.initState] inside [createState].
+  void onPreparation(AppStateValue state) {}
+
+  ///  Overriding [WidgetsBinding] after [AppStateValue.build] has been done.
+  void onReady(AppStateValue state) {}
+
+  @override
+  State<AppStateWidget> createState() => AppStateValue();
+}
+
+/// State child class of [AppStateWidget].
+class AppStateValue extends State<AppStateWidget> {
+  late Map<String, dynamic> data = widget.data(this);
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) => widget.onReady(this));
+    return widget.onStart(this);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.onPreparation(this);
+  }
+
+  @override
+  void dispose() {
+    widget.onFinish(this);
+    super.dispose();
+  }
 }
