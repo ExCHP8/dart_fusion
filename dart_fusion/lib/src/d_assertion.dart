feb4dd0c40772c3a1e46957f6a9b06ccd48f32a3
Nialixus <nialixus@gmail.com>
2023-12-06 17:50:54 +0700

    update assertion response

--- dart_fusion/lib/src/d_assertion.dart
+++ dart_fusion/lib/src/d_assertion.dart
@@ -1,60 +1,116 @@
 part of '../dart_fusion.dart';
 
-/// A utility class for performing response assertions.
+/// A utility class for performing assertions and validations.
 ///
-/// The [ResponseAssertion] class facilitates validating assertions based on
-/// boolean conditions. If the assertion fails, it throws a [ResponseException]
-/// with a response message and status code.
-class ResponseAssertion {
-  /// Constructs a [ResponseAssertion] with the provided parameters.
+/// The [Assert] class facilitates assertion checks based on boolean conditions.
+/// If the assertion fails, it throws an [Exception] with a provided message.
+class Assert extends DModel {
+  /// Constructs an [Assert] with the provided [assertion] and [message].
   ///
   /// The [assertion] parameter represents the boolean condition to check.
   ///
-  /// The [message] parameter specifies the message to be included in the response
+  /// The [message] parameter specifies the message to be included in the exception
   /// if the assertion fails.
-  ///
-  /// The [statusCode] parameter sets the HTTP status code for the response in case
-  /// the assertion fails. The default status code is 400 (Bad Request).
-  ResponseAssertion(
-    // ignore: avoid_positional_boolean_parameters
-    this.assertion,
-    this.message, {
-    this.statusCode = 400,
-  }) : _list = [] {
-    run();
+  Assert(this.assertion, this.message) : children = [] {
+    _run();
   }
 
-  /// Constructs a [ResponseAssertion] list with the provided [children].
+  /// Constructs an [Assert] instance with a list of child [Assert] instances.
   ///
-  /// The [children] parameter specifies the list of [ResponseAssertion] instances to be included in this [ResponseAssertion] list.
+  /// The [children] parameter specifies a list of [Assert] instances to be executed.
   ///
-  /// The [statusCode] parameter sets the HTTP status code for the response if any of the assertions in the [children] list fail.
-  /// The default status code is 400 (Bad Request).
-  ResponseAssertion.list({
-    required List<ResponseAssertion> children,
-    this.statusCode = 400,
-  })  : _list = children,
-        assertion = true,
-        message = '';
+  /// The [assertion] property is set to `true` by default.
+  ///
+  /// The [message] property is an empty string by default.
+  Assert.list({
+    required this.children,
+  })  : assertion = true,
+        message = '' {
+    _run();
+  }
 
-  /// The list of [ResponseAssertion] instances included in this [ResponseAssertion].
-  final List<ResponseAssertion> _list;
+  /// The list of child [Assert] instances to be executed.
+  final List<Assert> children;
 
   /// The boolean assertion condition to be checked.
   final bool assertion;
 
-  /// The message included in the response if the assertion fails.
+  /// The message included in the exception if the assertion fails.
   final String message;
+
+  /// Runs the assertion check.
+  void _run() {
+    if (!assertion && children.isEmpty) {
+      throw Exception(message);
+    }
+  }
+
+  ///
+  /// The [assertion] parameter represents the boolean condition to check.
+  ///
+  /// The [message] parameter specifies the message to be included in the exception
+  /// if the assertion fails.
+  ///
+  /// The [statusCode] parameter sets the HTTP status code for the response
+  /// if the assertion fails. The default status code is 400 (Bad Request).
+  static ResponseAssert response(
+    bool assertion,
+    String message, {
+    int statusCode = 400,
+  }) {
+    return ResponseAssert(
+      assertion,
+      message,
+      statusCode: statusCode,
+    );
+  }
+
+  static Assert fromJSON(JSON value) {
+    return Assert(
+      value.of('assertion', true),
+      value.of('message'),
+    );
+  }
+
+  @override
+  Assert copyWith({
+    bool? assertion,
+    String? message,
+  }) {
+    return Assert(
+      assertion ?? this.assertion,
+      message ?? this.message,
+    );
+  }
+
+  @override
+  JSON get toJSON => {
+        'assertion': assertion,
+        'message': message,
+        ...super.toJSON,
+      };
+}
+
+/// A specialized [Assert] class for handling response-related assertions.
+///
+/// The [ResponseAssert] class extends [Assert] and introduces functionality
+/// to handle assertions related to HTTP responses, including status code checks.
+class ResponseAssert extends Assert {
+  /// Constructs a [ResponseAssert] with the provided [assertion], [message], and [statusCode].
+  ///
+  /// The [statusCode] parameter sets the HTTP status code for the response
+  /// if the assertion fails. The default status code is 400 (Bad Request).
+  ResponseAssert(bool assertion, String message, {this.statusCode = 400})
+      : super(assertion, message) {
+    _run();
+  }
 
   /// The HTTP status code for the response if the assertion fails.
   final int statusCode;
 
-  /// Executes the assertion check.
-  ///
-  /// If the [assertion] fails, it throws a [ResponseException] with a response
-  /// containing the specified [statusCode] and [message].
-  void run() {
-    if (!assertion && _list.isEmpty) {
+  @override
+  void _run() {
+    if (!assertion) {
       throw ResponseException(
         response: Response.json(
           statusCode: statusCode,
@@ -64,5 +120,32 @@ class ResponseAssertion {
         ),
       );
     }
+  }
+
+  @override
+  JSON get toJSON => {
+        'status_code': statusCode,
+        ...super.toJSON,
+      };
+
+  static ResponseAssert fromJSON(JSON value) {
+    return ResponseAssert(
+      value.of('assertion', true),
+      value.of('message'),
+      statusCode: value.of('status_code'),
+    );
+  }
+
+  @override
+  ResponseAssert copyWith({
+    bool? assertion,
+    String? message,
+    int? statusCode,
+  }) {
+    return ResponseAssert(
+      assertion ?? this.assertion,
+      message ?? this.message,
+      statusCode: statusCode ?? this.statusCode,
+    );
   }
 }
--- dart_fusion/lib/src/d_typedefs.dart
+++ dart_fusion/lib/src/d_typedefs.dart
@@ -2,9 +2,3 @@ part of '../dart_fusion.dart';
 
 /// Define [JSON] Type as a shortcut for [Map<String, dynamic>].
 typedef JSON = Map<String, dynamic>;
-
-/// A typedef alias for [ResponseAssertion].
-///
-/// The [Assert] typedef creates an alias for the [ResponseAssertion] class,
-/// allowing the use of [Assert] as an alternative name for [ResponseAssertion].
-typedef Assert = ResponseAssertion;
